3eab200b5ea28f88d529fe6376bbeeb6f5079c50
toyobayashi <lifenglin314@outlook.com>
2020-08-17 18:17:07 +0800

    随便排一下版

--- miniprogram/components/domel/domel.js
+++ miniprogram/components/domel/domel.js
@@ -4,5 +4,13 @@ Component({
       type: Object,
       optionalTypes: [String]
     }
+  },
+  methods: {
+    _previewImg () {
+      wx.previewImage({
+        current: this.properties.node.attr.src,
+        urls: [this.properties.node.attr.src],
+      })
+    }
   }
 })
