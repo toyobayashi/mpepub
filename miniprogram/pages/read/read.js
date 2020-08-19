f7d55c7adeea78ba822c679217732b4250b5e7fc
toyobayashi <lifenglin314@outlook.com>
2020-08-19 10:27:07 +0800

    滚动高度

--- miniprogram/pages/read/read.js
+++ miniprogram/pages/read/read.js
@@ -18,11 +18,17 @@ function parseTree (spineIndex) {
   })
 }
 
+let scrollWaitUpdate = null
+let scrollTimer = 0
+
 Page({
   data: {
     toc: [],
     spineIndex: -1,
     tree: null,
+    mainHeight: 0,
+    percent: '0.00%',
+    scrollTop: 0,
     _domCache: {}
   },
   onLoad () {
@@ -32,14 +38,20 @@ Page({
     // const cache = getGlobal(GlobalKey.ZIP)
     console.log(book)
     console.log(info)
-    if (info) {
+    const q = wx.createSelectorQuery()
+    q.select('.main').boundingClientRect().exec((res) => {
+      console.log(res[0].height)
+
       this.setData({
-        toc: info.navigation.toc
+        ...(info ? { toc: info.navigation.toc } : {}),
+        mainHeight: res[0].height
       })
-    }
-    if (book) {
-      console.log(book.key())
-    }
+
+      if (book) {
+        console.log(book.key())
+      }
+    })
+
     // book.locations.generate(10000).then((locations) => {
     //   console.log(locations)
     //   console.log(book.locations.save())
@@ -50,7 +62,8 @@ Page({
   _showIndex () {
     this.setData({
       spineIndex: -1,
-      tree: null
+      tree: null,
+      scrollTop: 0
     })
   },
   _prev () {
@@ -59,42 +72,22 @@ Page({
       this._showIndex()
     } else {
       const spineIndex = this.data.spineIndex - 1
-      if (this.data._domCache[spineIndex]) {
-        this.setData({
-          spineIndex,
-          tree: this.data._domCache[spineIndex]
-        })
-      } else {
-        parseTree(spineIndex).then(tree => {
-          this.data._domCache[spineIndex] = tree
-          this.setData({
-            spineIndex,
-            tree
-          })
-        })
-      }
+      this._renderSpine(spineIndex)
     }
   },
   _next () {
     const info = getGlobal(GlobalKey.BOOK_INFO)
     const sections = info.spine.items
-    if (this.data.spineIndex >= sections.length - 1) return
+    if (this.data.spineIndex >= sections.length - 1) {
+      wx.showToast({
+        title: '已经到最后啦',
+        icon: 'none'
+      })
+      return
+    }
 
     const spineIndex = this.data.spineIndex + 1
-    if (this.data._domCache[spineIndex]) {
-      this.setData({
-        spineIndex,
-        tree: this.data._domCache[spineIndex]
-      })
-    } else {
-      parseTree(spineIndex).then(tree => {
-        this.data._domCache[spineIndex] = tree
-        this.setData({
-          spineIndex,
-          tree
-        })
-      })
-    }
+    this._renderSpine(spineIndex)
   },
   onTocTap (e) {
     const book = getGlobal(GlobalKey.BOOK)
@@ -113,19 +106,38 @@ Page({
       // TODO
       return
     }
+    this._renderSpine(spineIndex)
+  },
+  _renderSpine (spineIndex) {
     if (this.data._domCache[spineIndex]) {
       this.setData({
         spineIndex,
-        tree: this.data._domCache[spineIndex]
+        tree: this.data._domCache[spineIndex],
+        scrollTop: 0
       })
-    } else {
-      parseTree(spineIndex).then(tree => {
-        this.data._domCache[spineIndex] = tree
-        this.setData({
-          spineIndex,
-          tree
-        })
-      })
+      return Promise.resolve()
     }
+
+    return parseTree(spineIndex).then(tree => {
+      this.data._domCache[spineIndex] = tree
+      this.setData({
+        spineIndex,
+        tree,
+        scrollTop: 0
+      })
+    })
+  },
+  _updateScroll () {
+    this.setData(scrollWaitUpdate)
+    scrollWaitUpdate = null
+  },
+  _onScroll (e) {
+    const scrollable = e.detail.scrollHeight - this.data.mainHeight
+    const percent = (Math.floor(e.detail.scrollTop / scrollable * 10000) / 100).toFixed(2) + '%'
+    scrollWaitUpdate = {
+      percent
+    }
+    clearTimeout(scrollTimer)
+    scrollTimer = setTimeout(this._updateScroll, 200)
   }
 })
